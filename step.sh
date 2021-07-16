dea214caffddfc9bf3ce733adb78bc83f472a3f6
Imre Kelényi <imre.kelenyi@gmail.com>
2021-07-16 16:48:12 +0200

    pick a random song

--- step.sh
+++ step.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 set -e
-echo "go to dubstep: https://www.youtube.com/watch?v=YJVmu6yttiw"
+
+dubstep[0]="https://www.youtube.com/watch?v=YJVmu6yttiw"
+dubstep[1]="https://www.youtube.com/watch?v=WSeNSzJ2-Jw"
+dubstep[2]="https://www.youtube.com/watch?v=jK2aIUmmdP4"
+dubstep[3]="https://www.youtube.com/watch?v=GGKPiTFmzrw"
+
+size=${#dubstep[@]}
+index=$(($RANDOM % $size))
+
+echo "go to dubstep: ${dubstep[$index]}"
 echo "¯\_(\\\\ツ)_/¯"
